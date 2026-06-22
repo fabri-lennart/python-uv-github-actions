3fdb877e8da2b4905d01b90de7105d581abb382e
fabri lennart <fabrilennart@gmail.com>
2026-06-22 17:53:26 -0300

    fixing main variables, to start the etl process

--- etl/transformm.py
+++ etl/transformm.py
@@ -2,8 +2,10 @@
 import pandas as pd
 import json
 
+def transform_data():
 # pandas dataframe with  columns filtered
-df = pd.json_normalize(json.load(open('data/user_data.json')))
+    df = pd.json_normalize(json.load(open('data/user_data.json')))
+    #  save the dataframe as a csv fike 
+    df.to_csv('data/user_data_processed.csv', index=False)
 
-#  save the dataframe as a csv fike 
-df.to_csv('data/user_data_processed.csv', index=False)
+run_transform_process = transform_data()
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # lets import the etl modules to run the data extraction and transformation
-from etl.extract import extract_data
+import os
+from etl.extract import fetch_data
 from etl.transformm import transform_data
 from etl.load import upload_csv
 
 def run_pipeline():
-    extract_data()
+    fetch_data()
     transform_data()
-    upload_csv()
+    # Obtenemos la variable de entorno
+    bucket_name = os.environ.get("GCP_BUCKET_NAME")
+    
+    if bucket_name:
+        # Le pasamos los 3 argumentos que la función de etl/load.py exige
+        upload_csv(
+            bucket_name=bucket_name,
+            source_file="data/user_data_processed.csv",
+            dest_blob="gold/user_data_processed.csv"
+        )
+    else:
+        print("Error: No se encontró la variable GCP_BUCKET_NAME")
 
 if __name__ == "__main__":
     run_pipeline()
